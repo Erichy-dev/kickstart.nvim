2bc90f0105bb2ff09ed6d4428dbb7db92d4af491
Erichy-dev <erichnyaga1@gmail.com>
2025-03-01 19:25:07 +0300

    markdown preview keys

--- lua/custom/plugins/markdownpreview.lua
+++ lua/custom/plugins/markdownpreview.lua
@@ -3,7 +3,11 @@ return {
   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
   build = 'cd app && yarn install',
   init = function()
-    vim.g.mkdp_filetypes = { 'markdown' }
+    vim.g.mkdp_filetypes =
+      { 'markdown' },
+      -- Keymaps for Markdown preview
+      vim.keymap.set('n', '<leader>mp', '<cmd>MarkdownPreview<CR>', { desc = 'preview markdown file' }),
+      vim.keymap.set('n', '<leader>ms', '<cmd>MarkdownPreviewStop<CR>', { desc = 'preview markdown file' })
   end,
   ft = { 'markdown' },
 }
